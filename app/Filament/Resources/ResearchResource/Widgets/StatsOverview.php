6a8f9c7ab70500bfb906e21580749be73e9332dd
Rena Nario <relaneperez@gmail.com>
2022-08-17 15:52:09 +0800

    Filters

--- app/Filament/Resources/ResearchResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/ResearchResource/Widgets/StatsOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\ResearchResource\Widgets;
 
+
 use App\Models\Research;
 use Illuminate\Support\HtmlString;
 use Filament\Widgets\StatsOverviewWidget\Card;
@@ -12,6 +13,7 @@ class StatsOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
+            
             Card::make('Research Total', Research::all()->count()),
             Card::make('Recently Added', new HtmlString(implode("<br>", Research::latest()->take(5)->pluck('Title')->toArray()))),
            
